e1914554d101fbebeda7be398b4d1342fc1f6a20
Junhu2021 <87661225+Junhu2021@users.noreply.github.com>
2021-08-27 11:46:04 -0600

    Create Food Order.py
    Start from a simple python program

---
+++ Food Order.py
@@ -0,0 +1,63 @@
+## Ask the user's last name
+lastName = input('Please input your last name : \n')
+## define a funtion to return different types of toppings for the user to select
+def items(key):
+    if (key == 1):
+        return "Cheese"
+    elif (key == 2):
+        return "Pineapple"
+    elif (key == 3):
+        return "Pepperoni"
+    elif (key == 4):
+        return "Ham"
+    elif (key == 5):
+        return "poop"
+    else:
+        return "Done(Finished selecting toppings)"
+## create a key list for 6 options
+keys = [1,2,3,4,5,6]
+## create a dictionary for the prices based on the number of toppings
+price = {1:8,2:10, 3:12,4:14}
+## Create an empty topping list to put the user choosing
+toppingList=[]
+##create a list to put the option number of the chosen topping 
+numberTopping = []
+## create a varaible for the total payment
+tally = 0
+## creat a variable to put the number of topping the user chooses
+toppingNumber = 0
+choosing = True
+## Use while loops to print the topping types 
+while (choosing):
+    for i in range(0, len(keys)):
+        x = keys[i]
+        print(str(x) + ".) " + items(x))
+    choice = input('choose a topping:\n')
+## the user selects the “Done” option to exit. Also the program would exit if an invalid number is input
+    if (int(choice) == 6):
+        choosing = False
+    elif (int(choice) >6) or (int(choice) < 1):
+       choosing = False
+       print (' you input an invalid number')
+       exit()
+## calculate the total charge for four toppings or Everything = $14
+    elif(toppingNumber == 4) or (int(choice) == 5):
+        choosing = False
+        tally =  price [4]
+        toppingList.append(items(5))
+## use choose 1-3 topping and locate their relevant price
+    else:
+        toppingNumber = toppingNumber+1
+        tally = price[toppingNumber]
+        toppingList.append(items(int(choice)))
+## remove the slected toppings
+    keys.remove(int(choice))
+    numberTopping.append(int(choice))
+print("Hello, "+ lastName+ ". you chose :" )
+##print a listing of the topping they chose with choosing options sorted by choosing sequence
+for i in range(0, len(toppingList)):
+    print(str(numberTopping[i])+ ".) "+ toppingList[i])
+print("Your total comes to: ${:.2f}".format(tally,2))
+## program name: Jun Hu
+## Date program written : August 21,2021
+## descrition of purpose of program: generate a pizza toppings order for customers and let them choose and calculated the total charge by printing the receipt
